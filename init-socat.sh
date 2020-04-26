107dc5441682e53c03b503c9a0b8b7c95dd36c5b
Jake Keeys <jake@keeys.org>
2020-04-26 11:34:25 +0100

    socat reconnect

--- init-socat.sh
+++ init-socat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-socat pty,wait-slave,link=/socat/ttyACM0 tcp:${PRINTER_HOST} &
+socat pty,wait-slave,link=/socat/ttyACM0 tcp:${PRINTER_HOST},forever,interval=10,fork &
 exec docker-entrypoint.sh
